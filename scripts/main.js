f84830418715939ae156d535f882fefadf8fc10f
Maksim web <sayamonkoh@gmail.com>
2020-12-08 23:48:24 +1000

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const element = document.querySelector('#selectCustom');
+const choices = new Choices(element, {
+  searchEnabled: false,
+  itemSelectText: '',
+});
+
+const element2 = document.querySelector('#selectCustom-2');
+const choices2 = new Choices(element2, {
+  searchEnabled: false,
+  itemSelectText: '',
+});
+
+const element3 = document.querySelector('#selectCustom-3');
+const choices3 = new Choices(element3, {
+  searchEnabled: false,
+  itemSelectText: '',
+});
+
+const element4 = document.querySelector('#selectCustom-4');
+const choices4 = new Choices(element4, {
+  searchEnabled: false,
+  itemSelectText: '',
+});
+
+const element5 = document.querySelector('#selectCustom-5');
+const choices5 = new Choices(element5, {
+  searchEnabled: false,
+  itemSelectText: '',
+});
