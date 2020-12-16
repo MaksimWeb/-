87741820eff0a1311a0c178985996b420e3d17c5
Maksim web <sayamonkoh@gmail.com>
2020-12-16 11:10:17 +1000

    header finish

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,24 @@ bottomMenuIcon.forEach(el => el.addEventListener('click', () => {
     });
     bottomMenuDropDown.forEach(n => {
         if (bottomMenuIcon.indexOf(el) == bottomMenuDropDown.indexOf(n))
-        n.classList.toggle('menu-bottom__submenu_down')
+            n.classList.toggle('menu-bottom__submenu_down')
     });
 }))
+
+const searchInput = document.querySelector('.header__search-input');
+const searchRect = document.querySelector('.search__rect');
+const searchPath = document.querySelector('.search__path');
+
+searchInput.addEventListener('click', () => {
+    searchInput.style.color = "#C283F3";
+    searchRect.style.fill = "#C283F3";
+    searchPath.style.fill = "#C283F3";
+    console.log('s')
+});
+
+searchInput.addEventListener('input', () => {
+    if (searchInput.value == '') {
+        searchRect.style.fill = "white";
+        searchPath.style.fill = "white";
+    }
+})
