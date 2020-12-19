72b85f38d6e801c0f5b5cc03dc649a96c63af48a
Maksim web <sayamonkoh@gmail.com>
2020-12-19 17:42:00 +1000

    menu item styled

--- scripts/main.js
+++ scripts/main.js
@@ -12,11 +12,12 @@ bottomMenuIcon.forEach(el => el.addEventListener('click', () => {
         if (bottomMenuIcon.indexOf(el) == bottomMenuDropDown.indexOf(n))
             n.classList.toggle('menu-bottom__submenu_down')
     });
-}))
+}));
 
 
 // Поле поиска
 const searchInput = document.querySelector('.header__search-input');
+const searchForm = document.querySelector('.header__search-block');
 const searchRect = document.querySelector('.search__rect');
 const searchPath = document.querySelector('.search__path');
 
@@ -30,23 +31,34 @@ searchInput.addEventListener('input', () => {
     if (searchInput.value == '') {
         searchRect.style.fill = "white";
         searchPath.style.fill = "white";
-    }
-})
+    };
+});
 
 // Сслыка войти
 
 const entrance = document.querySelector('.entrance-link');
 const entranceIcon = document.querySelector('.entrance__logo');
+const entranceIconBorder = document.querySelector('.entrance');
 entrance.onmouseover = entrance.onmouseout = (event) => {
     if (event.type == 'mouseover') {
         entranceIcon.classList.add('entrance__logo_fill')
-    }
+    };
 
     if (event.type == 'mouseout') {
         entranceIcon.classList.remove('entrance__logo_fill')
-    }
+    };
 };
 
 entrance.addEventListener('mousedown', () => {
-        entranceIcon.classList.add('entrance__logo_active')
+    entranceIcon.classList.add('entrance__logo_active')
 });
+
+entrance.addEventListener('focus', () => {
+    entranceIconBorder.style.outline = "2px solid #7943A4";
+    entranceIconBorder.style.outlineOffset = "3px";
+});
+
+entrance.addEventListener('blur', () => {
+    entranceIconBorder.style.outline = "none";
+});
+
